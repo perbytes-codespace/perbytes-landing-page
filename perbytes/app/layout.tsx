97718bd2a1a4de5b8bc3d529a034505119b6ef85
Idelilah <ruthtirusew944@gmail.com>
2024-01-13 04:39:14 +0300

    add: count down timer

--- perbytes/app/layout.tsx
+++ perbytes/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={GeistMono.className}>{children}</body>
+      <body className={GeistSans.className}>{children}</body>
     </html>
   )
 }
--- perbytes/app/page.tsx
+++ perbytes/app/page.tsx
@@ -1,6 +1,51 @@
-import Image from 'next/image';
+'use client';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const countDownDate = new Date('January 30, 2024 23:59:59').getTime();
+  const [distance, setDistance] = useState(countDownDate - new Date().getTime());
+  const [prevDays, setPrevDays] = useState(0);
+  const [prevHours, setPrevHours] = useState(0);
+  const [prevMinutes, setPrevMinutes] = useState(0);
+  const [prevSeconds, setPrevSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      const newDistance = countDownDate - now;
+      setDistance(newDistance);
+
+      if (newDistance < 0) {
+        clearInterval(interval);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [countDownDate]);
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  useEffect(() => {
+    if (days !== prevDays) {
+      setPrevDays(days);
+    }
+    if (hours !== prevHours) {
+      setPrevHours(hours);
+    }
+    if (minutes !== prevMinutes) {
+      setPrevMinutes(minutes);
+    }
+    if (seconds !== prevSeconds) {
+      setPrevSeconds(seconds);
+    }
+  }, [days, hours, minutes, prevDays, prevHours, prevMinutes, prevSeconds, seconds]);
+
+
   return (
     <main className="flex min-h-screen flex-col items-center relative">
       <div
@@ -21,16 +66,39 @@ export default function Home() {
           <p className="text-lg px-4 border border-1 rounded-lg">Contact</p>
         </div> */}
       </div>
-
       <div className="relative flex flex-col align-center items-center justify-center my-32 px-[25rem] w-full">
         {/* Content */}
         <div className="z-10 text-center">
-          <p className="text-4xl font-semibold">
-            Perbytes. Coming Soon.
-          </p>
-          <p className='text-4xl font-semibold'>
-            Please stay tuned.
-          </p>
+          <p className="text-4xl font-semibold mb-4">Coming Soon. Stay tuned.</p>
+          <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+            <div className="flex flex-col">
+              <span className="countdown font-mono text-8xl">
+                <span style={{ "--value": days !== prevDays ? days : prevDays }}></span>
+              </span>
+              days
+            </div>
+            <span className='text-8xl'>:</span>
+            <div className="flex flex-col">
+              <span className="countdown font-mono text-8xl">
+                <span style={{ "--value": hours !== prevHours ? hours : prevHours }}></span>
+              </span>
+              hours
+            </div>
+            <span className='text-8xl'>:</span>
+            <div className="flex flex-col">
+              <span className="countdown font-mono text-8xl">
+                <span style={{ "--value": minutes !== prevMinutes ? minutes : prevMinutes }}></span>
+              </span>
+              min
+            </div>
+            <span className='text-8xl'>:</span>
+            <div className="flex flex-col">
+              <span className="countdown font-mono text-8xl">
+                <span style={{ "--value": seconds !== prevSeconds ? seconds : prevSeconds }}></span>
+              </span>
+              sec
+            </div>
+          </div>
         </div>
       </div>
       <footer className='flex align-center items-center text-white z-0 absolute bottom-0'>
