925a22738fd0464a652df7698061b52973cc05d6
Idelilah <ruthtirusew944@gmail.com>
2024-03-17 13:26:32 +0300

    fix: Footer

--- perbytes/app/components/footer.tsx
+++ perbytes/app/components/footer.tsx
@@ -4,6 +4,7 @@ import { IoIosArrowRoundUp } from "react-icons/io";
 import { FaFacebookF } from "react-icons/fa6";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa6";
+import Link from 'next/link';
 
 export const  Footer=() => {
     const year = new Date().getFullYear();
@@ -18,12 +19,12 @@ export const  Footer=() => {
         <div className="sm:flex justify-between px-6  lg:px-[220px] my-10">
         <div className="sm:w-1/2 flex flex-col gap-4">
         <div className="flex justify-between">
-            <a href="index.html" className="flex gap-2 items-center">
+            <Link href="/" className="flex gap-2 items-center">
                 <div className="h-8 w-6">
                     <Image className="h-8 w-6" width={50} height={50} src="/footer-logo.png" alt="logo" />
                 </div>
                 <span className="text-lg">Perbytes <span className=" font-semibold">Systems</span></span>
-            </a>
+            </Link>
             <div className="flex gap-2 items">
                 <div className="h-8 w-6">
                     <FaFacebookF />
@@ -46,10 +47,10 @@ export const  Footer=() => {
                 Company
             </p>
             <ul className="flex flex-col gap-2">
-                <li><a className="" href="about.html">About Us</a></li>
-                <li><a className="" href="about.html">Team</a></li>
-                <li><a className="" href="about.html">Services</a></li>
-                <li><a className="" href="contact.html">Contact Us</a></li>
+                <li><a className="" href="#">About Us</a></li>
+                <li><a className="" href="#">Team</a></li>
+                <li><a className="" href="#">Services</a></li>
+                <li><a className="" href="#">Contact Us</a></li>
             </ul>
     
         </div>
@@ -66,7 +67,7 @@ export const  Footer=() => {
         </div>
     </div>
     <div className="flex justify-between py-8 bg-[#043A4E] text-white items-center lg:px-[220px]">
-        <p className="sm:text-xl text-sm">PerBytes Technologies © {year}. <span className='hidden md:inline'>All Rights Reserved.</span></p>
+        <p className="sm:text-xl text-sm">PerBytes Systems, Inc © {year}. <span className='hidden md:inline'>All Rights Reserved.</span></p>
         <p className="sm:text-xl text-sm cursor-pointer flex gap-2 items-center" onClick={backToTop}>
         <IoIosArrowRoundUp /> Back to top
         </p>
